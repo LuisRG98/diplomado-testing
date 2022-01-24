3f728074fae3ee76acedbab151514464a334bac6
LuisRG98 <70450227+LuisRG98@users.noreply.github.com>
2022-01-24 15:50:37 -0400

    Add files via upload

--- part2.py
+++ part2.py
@@ -19,57 +19,56 @@ with open('Output.txt') as f:
 d = ast.literal_eval(data)
 
 
-# composerUrl = 'http://vindemo4.composer.dealer.com/website/as/vindemo4/vindemo4-admin/composer/index?deeplink=%2F&contentLocale=en_US&format=&__ssuMode=true#website'
 composerUrl = 'http://vindemo7.composer.dealer.com/website/as/vindemo7/vindemo7-admin/composer/index?deeplink=%2Fdealership%2Fstaff.htm&contentLocale=en_US&format=&__ssuMode=true#website'
 
 cms_site = 'http://vindemo7.cms.dealer.com/dealership/staff.htm'
 
-# driver.get(composerUrl)
-# userInput = driver.find_element(By.ID, 'username').send_keys(user)
-# sigInButton = driver.find_element(By.ID,'signIn').click()
-# time.sleep(2)
+driver.get(composerUrl)
+userInput = driver.find_element(By.ID, 'username').send_keys(user)
+sigInButton = driver.find_element(By.ID,'signIn').click()
+time.sleep(2)
 
-# passwInput = driver.find_element(By.ID,'password').send_keys(passw)
-# sigInButton = driver.find_element(By.ID,'signIn').click()
-
-
-# time.sleep(2)
+passwInput = driver.find_element(By.ID,'password').send_keys(passw)
+sigInButton = driver.find_element(By.ID,'signIn').click()
 
 
-# for info in d.values():
+time.sleep(2)
+
+
+for info in d.values():
     
-#     driver.get(composerUrl)
-#     time.sleep(5)
-#     driver.switch_to.frame('site-iframe')
-#     add =  driver.find_element(By.XPATH, "//button[text()='Create Staff Member']").click()
-#     time.sleep(2)
-#     driver.switch_to.default_content()
-#     new_name = driver.find_element(By.NAME, 'staffName').send_keys(info['name'])
-#     new_title = driver.find_element(By.NAME, 'staffTitle').send_keys(info['jobTitle'])
-#     new_department = driver.find_element(By.NAME, 'staffDepartment').send_keys(info['department'])
+    driver.get(composerUrl)
+    time.sleep(5)
+    driver.switch_to.frame('site-iframe')
+    add =  driver.find_element(By.XPATH, "//button[text()='Create Staff Member']").click()
+    time.sleep(2)
+    driver.switch_to.default_content()
+    new_name = driver.find_element(By.NAME, 'staffName').send_keys(info['name'])
+    new_title = driver.find_element(By.NAME, 'staffTitle').send_keys(info['jobTitle'])
+    new_department = driver.find_element(By.NAME, 'staffDepartment').send_keys(info['department'])
     
-#     if(info['phone'] !='no telephone present'):
-#         new_phone = driver.find_element(By.NAME, 'staffPhone').send_keys(info['phone'])
+    if(info['phone'] !='no telephone present'):
+        new_phone = driver.find_element(By.NAME, 'staffPhone').send_keys(info['phone'])
     
-#     if(info['email'] !='no email present'):
-#         new_email = driver.find_element(By.NAME, 'staffEmail').send_keys(info['email'])
-#         new_email = driver.find_element(By.NAME, 'staffEmail').send_keys(Keys.TAB, Keys.TAB, info['description'])
-#     else:
-#         new_email = driver.find_element(By.NAME, 'staffEmail').send_keys(Keys.TAB, Keys.TAB, info['description'])
+    if(info['email'] !='no email present'):
+        new_email = driver.find_element(By.NAME, 'staffEmail').send_keys(info['email'])
+        new_email = driver.find_element(By.NAME, 'staffEmail').send_keys(Keys.TAB, Keys.TAB, info['description'])
+    else:
+        new_email = driver.find_element(By.NAME, 'staffEmail').send_keys(Keys.TAB, Keys.TAB, info['description'])
    
-#     photo = driver.find_element(By.CLASS_NAME,'staffPhoto').click()
-#     time.sleep(3)
-#     carpetA = driver.find_element(By.XPATH, "//span[text()='Added DO NOT DELETE']").click() 
-#     time.sleep(5)
-#     filter = driver.find_element(By.XPATH, "//input[@class='x-form-text x-form-field x-form-text-clearable x-form-empty-field']")
-#     filter.send_keys(info['imageURL'])
-#     time.sleep(2)
-#     laimagen =  driver.find_element(By.XPATH, "//span[text()='"+info['imageURL']+"']")
-#     actionChains = ActionChains(driver)
-#     actionChains.double_click(laimagen).perform()    
-#     #driver.switch_to.default_content()
-#     save_button = driver.find_element(By.XPATH, "//button[text()='Save']").click()    
-#     time.sleep(10)
+    photo = driver.find_element(By.CLASS_NAME,'staffPhoto').click()
+    time.sleep(3)
+    carpetA = driver.find_element(By.XPATH, "//span[text()='Added DO NOT DELETE']").click() 
+    time.sleep(5)
+    filter = driver.find_element(By.XPATH, "//input[@class='x-form-text x-form-field x-form-text-clearable x-form-empty-field']")
+    filter.send_keys(info['imageURL'])
+    time.sleep(2)
+    laimagen =  driver.find_element(By.XPATH, "//span[text()='"+info['imageURL']+"']")
+    actionChains = ActionChains(driver)
+    actionChains.double_click(laimagen).perform()    
+    #driver.switch_to.default_content()
+    save_button = driver.find_element(By.XPATH, "//button[text()='Save']").click()    
+    time.sleep(10)
 
 #verify if the site has the correct information uploaded
 driver.get(cms_site)
@@ -94,8 +93,11 @@ for person in employees:
 
 for x_values, y_values in zip(d.values(), data2.values()):
 
-    print(x_values)
-    print(y_values)
+    if x_values['name'] == y_values['name'] and x_values['jobTitle'] == y_values['jobTitle'] and x_values['description'] == y_values['description'] and x_values['phone'] == y_values['phone'] and x_values['email'] == y_values['email']:
+        print('Empleado:' + x_values['name'] + 'tiene toda la informacion correcta') 
+    else:
+        print('Empleado:' + x_values['name'] + 'tiene datos incocistentes, por favor verificar la informacion')    
+    # print(y_values)
 
 
 driver.quit()
